df6839111768c40fc9d88fd8b675449f719555b6
Shaun Barnes <shaun.w.barnes@gmail.com>
2023-12-03 16:28:24 -0500

    Completed Day 3

---
+++ Day3/Program.cs
@@ -0,0 +1,44 @@
+﻿
+using System.Text;
+
+namespace Day3;
+
+public class Program
+{
+	public static EngineSchematic? schematic;
+
+	public static void Main(string[] args)
+	{
+		schematic = new EngineSchematic();
+
+		ReadSchematicFromFile();
+
+		schematic.FindParts();
+
+		var sum = schematic.Parts.Sum(p => p.Value);
+		Console.WriteLine($"The sum of the parts is {sum}");
+
+		var gearRatio = schematic.CalculateGearRatio();
+		Console.WriteLine($"The gear ratio is {gearRatio}");
+	}
+
+	private static void ReadSchematicFromFile()
+	{
+		var lines = File.ReadAllLines("EngineSchematic.txt");
+
+		var rowLength = lines[0].Count();
+
+		schematic.AddBlankRow(rowLength);
+
+		foreach (var line in lines)
+		{
+			StringBuilder sb = new StringBuilder(".");
+			sb.Append(line);
+			sb.Append('.');
+
+			schematic.AddRow(sb.ToString());
+		}
+
+		schematic.AddBlankRow(rowLength);
+	}
+}
