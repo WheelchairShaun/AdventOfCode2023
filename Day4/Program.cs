29bbf3c19d41776e5c056179fba19e111eb15b7a
Shaun Barnes <shaun.w.barnes@gmail.com>
2023-12-04 15:57:26 -0500

    Day 4 Part 1

--- Day4/Program.cs
+++ Day4/Program.cs
@@ -1,2 +1,59 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Day4;
+using System.Text.RegularExpressions;
+
+internal class Program
+{
+	private static List<ScratchCard> cards = new List<ScratchCard>();
+	private static void Main(string[] args)
+	{
+		ReadCardsFromFile();
+
+		var worth = cards.Sum(c => c.GetScore());
+		Console.WriteLine($"The scratchcards are worth {worth} points.");
+	}
+
+	private static int ReadCardId(string input)
+	{
+		string pattern = @"\d+";
+
+		var id = Regex.Match(input, pattern).Value;
+
+		return int.Parse(id);
+	}
+
+	private static int[] ReadNumbers(string input)
+	{
+		var pattern = @"\d+";
+
+		var t = Regex.Matches(input, pattern);
+
+		int[] numbers = new int[t.Count];
+
+		for (int i = 0; i < numbers.Length; i++)
+		{
+			numbers[i] = int.Parse(t[i].Value);
+		}
+
+		return numbers;
+	}
+
+	private static void ReadCardsFromFile()
+	{
+		var lines = File.ReadAllLines("cards.txt");
+
+		foreach (var line in lines)
+		{
+			var t = line.Split(':');
+
+			var id = ReadCardId(t[0]);
+
+			var r = t[1].Trim().Split('|');
+
+			var w = ReadNumbers(r[0].Trim());
+
+			var n = ReadNumbers(r[1].Trim());
+
+			cards.Add(new ScratchCard(id, w, n));
+		}
+	}
+}
---
+++ Day4/ScratchCard.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day4
+{
+	public class ScratchCard
+	{
+        public ScratchCard(int id, int[] winningNumbers, int[] numbers)
+        {
+            Id = id;
+			WinningNumbers = winningNumbers;
+			Numbers = numbers;
+		}
+
+		private int _id;
+
+		public int Id
+		{
+			get { return _id; }
+			set { _id = value; }
+		}
+
+		private int[] _numbers;
+
+		public int[] Numbers
+		{
+			get { return _numbers; }
+			set { _numbers = value; }
+		}
+
+		private int[] _winning;
+
+		public int[] WinningNumbers
+		{
+			get { return _winning; }
+			set { _winning = value; }
+		}
+
+		public int GetScore()
+		{
+			int n = Numbers.Intersect(WinningNumbers).Count();
+
+			if (n > 0)
+			{
+				return (int) Math.Pow(2, n - 1);
+			}
+
+			return 0;
+		}
+	}
+}
